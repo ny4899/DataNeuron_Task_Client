5a6fd7782775f7ceb8bcd6d05895390f6ad0c7b0
neeleshy <neeleshy@idreameducation.org>
2024-04-26 13:22:46 +0530

    initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Resizable } from "re-resizable";
+import "./App.css";
 
-function App() {
+const Box = ({ id, txt, width, height, onResize }) => (
+  <Resizable
+    defaultSize={{ width, height }}
+    className="resizable_container"
+    onResize={(e, direction, ref, d) => {
+      console.log(e)
+      console.log(direction)
+      console.log(ref)
+      console.log(d)
+      onResize(id, d.width)}}
+  >
+    {txt}
+  </Resizable>
+);
+
+const App = () => {
+  const [boxes, setBoxes] = useState([
+    { id: "001", width: "50%", height: "50%", txt: "001" },
+    { id: "002", width: "50%", height: "50%", txt: "002" },
+    { id: "003", width: "100%", height: "50%", txt: "003" },
+  ]);
+
+  const handleResize = (id, width) => {
+    const updatedBoxes = boxes.map((box) =>
+      box.id === id ? { ...box, width } : box
+    );
+    setBoxes(updatedBoxes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_wrapper">
+      {boxes.map((box) => (
+        <Box
+          key={box.id}
+          id={box.id}
+          txt={box.txt}
+          width={box.width}
+          height={box.height}
+          onResize={handleResize}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
